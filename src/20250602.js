90e5524d7c9562471a84063134ad6afa8d4a3c27
Liayeong <sesil2120@gmail.com>
2025-06-04 14:53:23 +0900

    20250604 두번쨰

--- src/20250602.js
+++ src/20250602.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Route, Routes,Link } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import Home from './components/Home'
 import About from './components/About'
 import Head from './components/Head'
